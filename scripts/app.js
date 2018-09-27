e51ce46d725fbaec5b8d938979ced388f14a5c1d
Manuel Venegas <manuelalexander08@gmail.com>
2018-09-27 15:28:48 -0500

    Agregamos nuevo metodo para agregar HTML desde JAvaScript

--- scripts/app.js
+++ scripts/app.js
@@ -20,3 +20,18 @@ function agregar(){
 	item.appendChild(texto);
 	lista.appendChild(item);
 }
+
+/*
+var lista= document.getElementById('lista_mercado');
+console.log(lista.parentNode);
+*/
+
+var elementos= document.getElementsByTagName("li");
+var lista= elementos[0].parentNode;
+var item= document.createElement("li");
+var texto= document.createTextNode("Ejemplo");
+item.appendChild(texto);
+lista.insertBefore(item, elementos[0]);
+
+item.innerHTML= "<p>otro ejemplo</p>";
+lista.replaceChild(item, elementos[0]);
